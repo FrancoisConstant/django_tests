3f94ac3320c6e966422ab90595c5437bddd87348
François Constant <francois.constant@gmail.com>
2016-09-13 18:11:52 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-tests',
-    version='0.1.1',
+    version='0.1',
     packages=['django_tests'],
     include_package_data=True,
     description='Utility classes for Django tests.',
