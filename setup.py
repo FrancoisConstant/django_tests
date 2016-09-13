f260a12f629fd4ac93fae44140cde708235f40cf
François Constant <francois.constant@gmail.com>
2016-09-13 18:04:36 +1000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-tests',
+    version='0.2.2',
+    packages=['django_tests'],
+    include_package_data=True,
+    description='Utility classes for Django tests.',
+    long_description=README,
+    url='https://github.com/FrancoisConstant/django_tests',
+    author='Francois Constant',
+    author_email='francois.constant@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
